1403914cc3b55dee6fb4c7d27cd56755073760d1
lajixiaowang <2073893883@qq.com>
2019-07-17 11:12:15 +0800

    啊啊这是最后一次了

--- JD_project/php/regist1.php
+++ JD_project/php/regist1.php
@@ -13,12 +13,12 @@
     $username = $_GET['username'];
     $sql= "select * from usertable where username='$username'";
     $result= $conn->query($sql);
-   
     if($result->num_rows>0){  
        echo '{"mes":"抱歉，用户名已存在","has":true}';
     }else{
        echo '{"mes":"该用户名可以注册","has":false}';
     };
     ?>
+    
 
 
